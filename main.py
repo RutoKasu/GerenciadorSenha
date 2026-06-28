d9824e76e47ea996e39a9f14ebebece99f0e51ce
RutoKasu <rainierlima739@gmail.com>
2026-06-28 12:25:36 -0300

    implementação inicial da função de login

--- main.py
+++ main.py
@@ -3,32 +3,36 @@ import time
 
 rodando = True
 
-while rodando:
-    print("=========")
-    print("Menu: \n[1] Criar Banco de Dados \n[2] Consultar Banco\n[3] Inserir dados\n[4] Deletar Banco\n[5] Deletar Usuário\n[0] Sair")
-    try:
-        choice = int(input("Escolha uma opção: "))
-    except:
-        print("Valor indisponivel!")
-        continue
+print("===== LOGIN =====")
+login = input("Login:")
+senha = input("Senha:")
+if login == "root" and senha == "0000":
 
-    match choice:
-        case 1:
-            criar()
-        case 2:
-            listar_user()
-        case 3:
-            try:
+    while rodando:
+        print("=========")
+        print("Menu: \n[1] Criar Banco de Dados \n[2] Consultar Usuários\n[3] Inserir Usuário\n[4] Deletar Banco\n[5] Deletar Usuário\n[0] Sair")
+        try:
+            choice = int(input("Escolha uma opção: "))
+        except:
+            print("Valor indisponivel!")
+            continue
+
+        match choice:
+            case 1:
+                criar()
+            case 2:
+                listar_user()
+            case 3:
                 inserir_user()
-            except:
-                print("Ocorreu um erro no banco de dados!")
-        case 4:
-            deletar_banco()
-        case 5:
-            listar_user()
-            deletar_user()
-        case 0:
-            rodando = False
-            print("Finalizando Programa...")
-            time.sleep(5)
-            print("Programa finalizado!")
+            case 4:
+                deletar_banco()
+            case 5:
+                listar_user()
+                deletar_user()
+            case 0:
+                rodando = False
+                print("Finalizando Programa...")
+                time.sleep(5)
+                print("Programa finalizado!")
+else:
+    print("Login ou senha incorretos")
